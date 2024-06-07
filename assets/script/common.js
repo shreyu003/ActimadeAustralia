800574026b5280a84a00f05b8f89de434f92f3b4
shreyu003 <actoscriptdeveloper12@gmail.com>
2024-06-07 17:26:58 +0530

    header update by shreyu

--- assets/script/common.js
+++ assets/script/common.js
@@ -1,13 +1,40 @@
-var announceBarRemovebtn = document.querySelector('#announceBarRemoveBtn');
-var announceBar = document.querySelector('#announceBar');
-
-announceBarRemovebtn.addEventListener('click', () =>{
-    announceBar.remove();
-})
-
 var swiper = new Swiper(".heroSectionSlider", {
     navigation: {
       nextEl: ".swiper-button-next.hero_slider_btn-next",
       prevEl: ".swiper-button-prev.hero_slider_btn-prev",
     },
 });
+var swiper = new Swiper(".category-slider", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    freeMode: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".category-button-next",
+        prevEl: ".category-button-prev",
+    },
+});
+
+var announceBarRemovebtn = document.querySelector('#announceBarRemoveBtn');
+var announceBar = document.querySelector('#announceBar');
+
+announceBarRemovebtn.addEventListener('click', () =>{
+    announceBar.remove();
+})
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("product-tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("product-tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
