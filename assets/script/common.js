47242973eae5b0a4e45b0bc1bc8e28dc629b3e1d
Actoscript10 <actoscriptdevelop10@gmail.com>
2024-06-08 10:09:53 +0530

    changes by manthan

--- assets/script/common.js
+++ assets/script/common.js
@@ -6,6 +6,7 @@ var swiper = new Swiper(".heroSectionSlider", {
 });
 var swiper = new Swiper(".category-slider", {
     slidesPerView: 6,
+    slidesPerrow: 1,
     spaceBetween: 30,
     freeMode: true,
     autoplay: {
@@ -24,17 +25,3 @@ var announceBar = document.querySelector('#announceBar');
 announceBarRemovebtn.addEventListener('click', () =>{
     announceBar.remove();
 })
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("product-tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("product-tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-document.getElementById("defaultOpen").click();
