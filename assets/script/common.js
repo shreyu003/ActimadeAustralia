b4564e4b64145d65dfd91c10d866a5c09d8b2183
hnsutariya <hnsutariya>
2024-06-10 17:38:32 +0530

    changes by het

--- assets/script/common.js
+++ assets/script/common.js
@@ -26,3 +26,22 @@ var announceBar = document.querySelector('#announceBar');
 announceBarRemovebtn.addEventListener('click', () =>{
     announceBar.remove();
 })
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
