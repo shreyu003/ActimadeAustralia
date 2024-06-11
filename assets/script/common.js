e0bbc9481c6983767450ad527fb7f91670720abf
hnsutariya <hnsutariya>
2024-06-11 12:14:39 +0530

    swiper in product page

--- assets/script/common.js
+++ assets/script/common.js
@@ -1,24 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
-});
-<<<<<<< HEAD
-var swiper2 = new Swiper(".mySwiper2", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
-=======
-var swiper = new Swiper(".category-slider", {
+var categorySlider = new Swiper(".category-slider", {
     slidesPerView: 2,
     slidesPerrow: 1,
     spaceBetween: 30,
@@ -32,7 +12,7 @@ var swiper = new Swiper(".category-slider", {
         nextEl: ".category-button-next",
         prevEl: ".category-button-prev",
     },
-<<<<<<< HEAD
+
     breakpoints: {
         320: {
             slidesPerView: 2,
@@ -51,43 +31,25 @@ var swiper = new Swiper(".category-slider", {
             spaceBetween: 20
         }
   }
-=======
-<<<<<<< HEAD
-});
-=======
->>>>>>> 08ca4e61a4a243e2b81d636debda889e4500125e
->>>>>>> 4cd96ac73bfa0d39059e685b9f36971e2d689329
 });
 
-var announceBarRemovebtn = document.querySelector('#announceBarRemoveBtn');
-var announceBar = document.querySelector('#announceBar');
 
-announceBarRemovebtn.addEventListener('click', () =>{
-    announceBar.remove();
-})
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-=======
-var swiper = new Swiper(".mySwiper", {
+var productMainBlock = new Swiper(".product_main_block", {
     loop: true,
     spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
   });
-  var swiper2 = new Swiper(".mySwiper2", {
+  var productImgSlider = new Swiper(".product_img_slider", {
     loop: true,
     spaceBetween: 10,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next.prd-slider-next",
+      prevEl: ".swiper-button-prev.prd-slider-prev",
     },
     thumbs: {
-      swiper: swiper,
+      swiper: productMainBlock,
     },
   });
->>>>>>> ea37361c68a9cca2a9b4c36521082887de131766
->>>>>>> 08ca4e61a4a243e2b81d636debda889e4500125e
->>>>>>> 4cd96ac73bfa0d39059e685b9f36971e2d689329
+
