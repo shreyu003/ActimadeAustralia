03271120e3753095c411d4d09989488e1c85d140
shreyu003 <actoscriptdeveloper12@gmail.com>
2024-06-11 13:30:11 +0530

    changes by shreyu

--- assets/script/common.js
+++ assets/script/common.js
@@ -1,73 +1,51 @@
 var categorySlider = new Swiper(".category-slider", {
-    slidesPerView: 2,
-    slidesPerrow: 1,
-    spaceBetween: 30,
-    freeMode: true,
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
+  slidesPerView: 6,
+  slidesPerrow: 1,
+  spaceBetween: 30,
+  freeMode: true,
+  loop: true,
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".category-button-next",
+    prevEl: ".category-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
     },
-    navigation: {
-        nextEl: ".category-button-next",
-        prevEl: ".category-button-prev",
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 20
     },
-
-    breakpoints: {
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 20
-        },
-        480: {
-            slidesPerView: 3,
-            spaceBetween: 20
-        },
-        640: {
-            slidesPerView: 4,
-            spaceBetween: 20
-        },
-        1024: {
-            slidesPerView: 6,
-            spaceBetween: 20
-        }
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 20
+    }
   }
-<<<<<<< HEAD
-
 });
-
-
-var announceBarRemovebtn = document.querySelector('#announceBarRemoveBtn');
-var announceBar = document.querySelector('#announceBar');
-
-announceBarRemovebtn.addEventListener('click', () =>{
-    announceBar.remove();
-})
-
-var swiper = new Swiper(".mySwiper", {
-=======
-});
-
-
 var productMainBlock = new Swiper(".product_main_block", {
->>>>>>> eb562c00760e3f3c1b8b9f01dd2da5b1633908a4
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
   var productImgSlider = new Swiper(".product_img_slider", {
-    loop: true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next.prd-slider-next",
-      prevEl: ".swiper-button-prev.prd-slider-prev",
-    },
-    thumbs: {
-      swiper: productMainBlock,
-    },
-  });
-<<<<<<< HEAD
-=======
-
->>>>>>> eb562c00760e3f3c1b8b9f01dd2da5b1633908a4
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next.prd-slider-next",
+    prevEl: ".swiper-button-prev.prd-slider-prev",
+  },
+  thumbs: {
+    swiper: productMainBlock,
+  },
+});
