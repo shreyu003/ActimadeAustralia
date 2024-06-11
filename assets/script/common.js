6d76961074d6e8f91777b90eabcc92828035924b
shreyu003 <actoscriptdeveloper12@gmail.com>
2024-06-11 13:31:23 +0530

    changes by shreyu

--- assets/script/common.js
+++ assets/script/common.js
@@ -30,39 +30,7 @@ var categorySlider = new Swiper(".category-slider", {
       spaceBetween: 20
     }
   }
-<<<<<<< HEAD
-});
-var productMainBlock = new Swiper(".product_main_block", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-  var productImgSlider = new Swiper(".product_img_slider", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next.prd-slider-next",
-    prevEl: ".swiper-button-prev.prd-slider-prev",
-  },
-  thumbs: {
-    swiper: productMainBlock,
-  },
-});
-=======
 
-});
-
-
-var announceBarRemovebtn = document.querySelector('#announceBarRemoveBtn');
-var announceBar = document.querySelector('#announceBar');
-
-announceBarRemovebtn.addEventListener('click', () =>{
-    announceBar.remove();
-})
-
-var swiper = new Swiper(".mySwiper", {
 });
 
 
@@ -99,4 +67,3 @@ var productMainBlock = new Swiper(".product_main_block", {
         document.getElementById('quantity').value = quantity;
     }
 }
->>>>>>> 3ce844fb8732a60bcb77f9ce4fea3786faf78206
