a7c78b642ac7bc90a91d66a53f59983a11019f8a
shreyu003 <actoscriptdeveloper12@gmail.com>
2024-06-12 15:37:11 +0530

    changes done all by shreyu

--- assets/script/custome-javascript.js
+++ assets/script/custome-javascript.js
@@ -1,10 +1,3 @@
-// var announceBarRemovebtn = document.querySelector('#announceBarRemoveBtn');
-// var announceBar = document.querySelector('#announceBar');
-
-// announceBarRemovebtn.addEventListener('click', () =>{
-//     announceBar.remove();
-// })
-
 // const body = document.getElementsByTagName('body');
 // const mblMenuOpnBtn = document.querySelector('#mobileMenuOpenBtn');
 // const mblMenuClsBtn = document.querySelector('#mobileMenuCloseBtn');
--- assets/script/getHeader&Footer.js
+++ assets/script/getHeader&Footer.js
@@ -10,3 +10,14 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error("Error loading header:", error);
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
+    })
+    .catch(error => {
+        console.error("Error loading footer:", error);
+    });
+});
