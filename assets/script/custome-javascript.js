b10e623961adabc80f0dc487424d113273b28c90
shreyu003 <actoscriptdeveloper12@gmail.com>
2024-06-12 18:34:19 +0530

    product page done by sr

--- assets/script/custome-javascript.js
+++ assets/script/custome-javascript.js
@@ -1,38 +1,25 @@
-// const body = document.getElementsByTagName('body');
-// const mblMenuOpnBtn = document.querySelector('#mobileMenuOpenBtn');
-// const mblMenuClsBtn = document.querySelector('#mobileMenuCloseBtn');
-// const mblMenu = document.querySelector('.mobile_menu_wrapper');
+const body = document.getElementsByTagName('body');
+const mblMenuOpnBtn = document.querySelector('#mobileMenuOpenBtn');
+const mblMenuClsBtn = document.querySelector('#mobileMenuCloseBtn');
+const mblMenu = document.querySelector('.mobile_menu_wrapper');
 
-// mblMenuOpnBtn.addEventListener('click', () => {
-//     mblMenu.style.left = '0%';
-//     body.style.overflowY = 'hidden';
-// });
+mblMenuOpnBtn.addEventListener('click', () => {
+    mblMenu.style.left = '0%';
+});
 
-// mblMenuClsBtn.addEventListener('click', () => {
-//     mblMenu.style.left = '-100%';
-// });
+mblMenuClsBtn.addEventListener('click', () => {
+    mblMenu.style.left = '-100%';
+});
 
 
-// const cartOpnBtn = document.querySelector('#cartOpenBtn');
-// const cartClsBtn = document.querySelector('#cartCloseBtn');
-// const sliderCart = document.querySelector('.cart_wrapper');
+const cartOpnBtn = document.querySelector('#cartOpenBtn');
+const cartClsBtn = document.querySelector('#cartCloseBtn');
+const sliderCart = document.querySelector('.cart_wrapper');
 
-// cartOpnBtn.addEventListener('click', () => {
-//     sliderCart.style.right = '0%';
-// });
+cartOpnBtn.addEventListener('click', () => {
+    sliderCart.style.right = '0%';
+});
 
-// cartClsBtn.addEventListener('click', () => {
-//     sliderCart.style.right = '-100%';
-// });
-
-// let quantity = 1;
-// function increment() {
-//     quantity++;
-//     document.getElementById('quantity').value = quantity;
-// }
-// function decrement() {
-//     if (quantity > 0) {
-//         quantity--;
-//         document.getElementById('quantity').value = quantity;
-//     }
-// }
+cartClsBtn.addEventListener('click', () => {
+    sliderCart.style.right = '-100%';
+});
