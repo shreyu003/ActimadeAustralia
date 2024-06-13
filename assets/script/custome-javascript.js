34e59cfe4f57dd96d4812e43deaa7c1c58ce9d88
shreyu003 <actoscriptdeveloper12@gmail.com>
2024-06-13 12:31:39 +0530

    product and header page done by shreyu

--- assets/script/custome-javascript.js
+++ assets/script/custome-javascript.js
@@ -1,52 +1,52 @@
+$(document).ready(function(){
+  $('.mobileMenuOpenBtn').click(function(){
+    $('.mobile_menu_wrapper').show();
+    setTimeout(function(){
+      $('.mobile_menu').css('left', '0%');
+    }, 300);
+  });
 
-// const body = document.getElementsByTagName('body');
-// const mblMenuOpnBtn = document.querySelector('#mobileMenuOpenBtn');
-// const mblMenuClsBtn = document.querySelector('#mobileMenuCloseBtn');
-// const mblMenu = document.querySelector('.mobile_menu_wrapper');
+  $('.mobileMenuCloseBtn').click(function(){
+    $('.mobile_menu').css('left', '-100%');
+    setTimeout(function(){
+      $('.mobile_menu_wrapper').hide();
+    }, 300);
+  });
+  
+  $('.cartOpenBtn').click(function(){
+    $('.cart_wrapper').show();
+    setTimeout(function(){
+      $('.cart_block').css('right', '0%');
+    }, 300);
+  });
 
-// mblMenuOpnBtn.addEventListener('click', () => {
-//     mblMenu.style.left = '0%';
-//     body.style.overflowY = 'hidden';
-// });
-
-// mblMenuClsBtn.addEventListener('click', () => {
-//     mblMenu.style.left = '-100%';
-// });
-
-
-// const cartOpnBtn = document.querySelector('#cartOpenBtn');
-// const cartClsBtn = document.querySelector('#cartCloseBtn');
-// const sliderCart = document.querySelector('.cart_wrapper');
-
-// cartOpnBtn.addEventListener('click', () => {
-//     sliderCart.style.right = '0%';
-// });
-
-// cartClsBtn.addEventListener('click', () => {
-//     sliderCart.style.right = '-100%';
-// });
-
-var printBtn = document.querySelector('#print-btn');
-printBtn.addEventListener('click', () => {
-    print();
-})
-
-var shareBtn = document.querySelector('#share-btn');
-shareBtn.addEventListener("click", function () {
-    navigator.share({
-      url: document.URL,
-      title: document.title,
-    });
+  $('.cartCloseBtn').click(function(){
+    $('.cart_block').css('right', '-100%');
+    setTimeout(function(){
+      $('.cart_wrapper').hide();
+    }, 300);
+  });
 });
 
-// let quantity = 1;
-// function increment() {
-//     quantity++;
-//     document.getElementById('quantity').value = quantity;
-// }
-// function decrement() {
-//     if (quantity > 0) {
-//         quantity--;
-//         document.getElementById('quantity').value = quantity;
-//     }
-// }
+function printBtn(){
+  print();
+}
+
+function shareBtn(){
+  navigator.share({
+    url: document.URL,
+    title: document.title,
+  });
+}
+
+var quantity = 1;
+function increment() {
+  quantity++;
+  document.getElementById('quantity').value = quantity;
+}
+function decrement() {
+  if (quantity > 1) {
+    quantity--;
+    document.getElementById('quantity').value = quantity;
+  }
+}
