4e553848078ad24143440ff21f73cbd76f67d7da
shreyu003 <actoscriptdeveloper12@gmail.com>
2024-06-13 14:28:58 +0530

    header and footer loader done by shreyu

--- assets/script/getHeader&Footer.js
+++ assets/script/getHeader&Footer.js
@@ -1,23 +1,2 @@
-// $("#header").load("header.html");
-// $("#footer").load("footer.html");
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("header.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("header").innerHTML = data;
-    })
-    .catch(error => {
-        console.error("Error loading header:", error);
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("footer.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("footer").innerHTML = data;
-    })
-    .catch(error => {
-        console.error("Error loading footer:", error);
-    });
-});
+$("#header").load("header.html");
+$("#footer").load("footer.html");
